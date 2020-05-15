99980539a0eb80972c09679fafad4625c941f48e
汪梦玲 <mengling_wang@qq.com>
2020-05-15 12:14:38 +0800

    add

--- src/main/java/application/com/cn/config/RestTemplateConfig.java
+++ src/main/java/application/com/cn/config/RestTemplateConfig.java
@@ -8,11 +8,17 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    /**
+     * create restTemplate bean
+     * @return
+     */
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
         HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        // 设置读取超时时间
         clientHttpRequestFactory.setReadTimeout(60 * 1000);
+        // 设置连接超时时间
         clientHttpRequestFactory.setConnectTimeout(60 * 1000);
         restTemplate.setRequestFactory(clientHttpRequestFactory);
         return restTemplate;
