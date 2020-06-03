4e912e3d504181045ae1d3a180878ab339dba6b7
汪梦玲 <mengling_wang@qq.com>
2020-06-03 20:12:39 +0800

    添加ssl

--- src/main/java/application/com/cn/config/RestTemplateConfig.java
+++ src/main/java/application/com/cn/config/RestTemplateConfig.java
@@ -1,5 +1,11 @@
 package application.com.cn.config;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -20,6 +26,15 @@ public class RestTemplateConfig {
         clientHttpRequestFactory.setReadTimeout(60 * 1000);
         // 设置连接超时时间
         clientHttpRequestFactory.setConnectTimeout(60 * 1000);
+        try {
+            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+                    SSLContexts.custom().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build(),
+                    NoopHostnameVerifier.INSTANCE);
+            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+            clientHttpRequestFactory.setHttpClient(httpClient);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         restTemplate.setRequestFactory(clientHttpRequestFactory);
         return restTemplate;
     }
